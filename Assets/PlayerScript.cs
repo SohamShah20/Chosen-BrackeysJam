21205b838a288216457d0fa24d8f787e4cb05d9b
SohamShah20 <127046502+SohamShah20@users.noreply.github.com>
2023-08-22 15:04:10 +0530

    basic character movement
    Movement directly modifies 'transform.position', could not develop satisfactory movement using 'playerBody.velocity'

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody2D playerBody;
+    public float speed;
     // Start is called before the first frame update
+
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
@@ -13,6 +16,21 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 }
